b358665d21011b3a879db59010513078515d28c5
ZRong <1043778024@qq.com>
2023-01-26 12:54:47 +0800

    commit install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+password=$1
+files_dir="./auto_install"
+tar_file_name="auto_install.tar.gz"
+
+clear_all() {
+    rm -rf ${tar_file_name}
+    rm -rf $0
+}
+
+get_files() {
+    git clone https://github.com/Captain-ZR/auto_install.git
+    
+    rm -rf ${files_dir}/install.sh
+    mv ${files_dir}/${tar_file_name} ./
+    rm -rf ${files_dir}    
+}
+
+decompress() {
+    #tar -zcvf - auto_install/ | openssl aes-256-ecb -pbkdf2 -k $password -out auto_install.tar.gz	#加密压缩
+    openssl aes-256-ecb -d -k $password -pbkdf2 -in ./${tar_file_name} | tar -zxvf -			#解密压缩
+}
+
+execute() {
+    clear_all
+    mv ${files_dir}/auto_install.sh ./
+    ./auto_install.sh
+}
+
+get_files
+decompress
+execute
